bafc2eded59e335a506c54da2c4d15d288f363fd
4keezix <basile.parrain03@gmail.com>
2024-09-30 14:52:55 +0200

    Début de la page Accueil
    Il faut:
    - Finir le bouton play
    - Ajouter un footer
    - Remplir la page

---
+++ html/style/style_accueil.js
@@ -0,0 +1,13 @@
+const headerLinks = document.querySelectorAll('.header-link');
+
+headerLinks.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        link.style.transition = 'transform 0.5s ease-out';
+        link.style.transform = 'scale(1.2)';
+    });
+
+    link.addEventListener('mouseleave', () => {
+        link.style.transition = 'transform 0.8s ease-in';
+        link.style.transform = 'scale(1)';
+    });
+});

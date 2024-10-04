489f92f66e5a91d830d78280930de30bbb8ffdb8
Basile <basile.parrain03@gmail.com>
2024-10-04 15:27:41 +0200

    Ajustements footer, et page accueil compatible plusieurs dimensions

---
+++ html/style/script_footer.js
@@ -0,0 +1,23 @@
+const footerLinks = document.querySelectorAll('.footer-link');
+
+footerLinks.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        link.style.transition = 'transform 1s ease-out';
+        link.style.transform = 'scale(1.2)';
+    });
+
+    link.addEventListener('mouseleave', () => {
+        if (!link.classList.contains('active')) {
+            link.style.transition = 'transform 0.6s ease-in';
+            link.style.transform = 'scale(1)';
+        }
+    });
+});
+
+const currentLocation = window.location.href;
+
+footerLinks.forEach(link => {
+    if (link.href === currentLocation) {
+        link.classList.add('active');
+    }
+});

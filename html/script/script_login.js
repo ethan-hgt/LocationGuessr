4b59a6fba4accd0e5dd011ee05597fb1eaa80866
ethan <ethan.hugerot@hotmail.fr>
2024-10-12 23:59:57 +0200

    Modification finale Login
    Il reste plus qu'a faire comme il faut le back et login inscription est niquel

--- html/script/script_login.js
+++ html/script/script_login.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     updateHeader();
-
     $('#inscriptionForm').on('submit', handleInscriptionSubmit);
     $('#connexionForm').on('submit', handleConnexionSubmit);
 });
@@ -9,7 +8,6 @@ function switchTab(tab) {
     const inscriptionForm = document.getElementById('inscriptionForm');
     const connexionForm = document.getElementById('connexionForm');
     const tabSlider = document.querySelector('.tab-slider');
-    
     if (tab === 'connexion') {
         inscriptionForm.style.display = 'none';
         connexionForm.style.display = 'flex';
@@ -24,7 +22,6 @@ function switchTab(tab) {
 function updateHeader() {
     const userName = localStorage.getItem('userFirstName');
     const rightHeader = document.querySelector('.right-header');
-    
     if (userName) {
         rightHeader.innerHTML = `<span>${userName} (<a href="#" class="logout-link" onclick="logout(); return false;">Déconnexion</a>)</span>`;
     } else {
@@ -36,15 +33,12 @@ function logout() {
     localStorage.removeItem('userFirstName');
     updateHeader();
     alert('Vous avez été déconnecté.');
-
     window.location.href = 'accueil.html';
 }
 
 function handleInscriptionSubmit(event) {
     event.preventDefault();
-
-    const firstName = document.getElementById('prenom').value;
-    const lastName = document.getElementById('nom').value;
+    const username = document.getElementById('usernameInscription').value;
     const email = document.getElementById('emailInscription').value;
     const password = document.getElementById('passwordInscription').value;
     const confirmPassword = document.getElementById('confirmPasswordInscription').value;
@@ -55,21 +49,18 @@ function handleInscriptionSubmit(event) {
     }
 
     const users = JSON.parse(localStorage.getItem('users')) || [];
-    
-    const userExists = users.some(user => user.email === email);
+    const userExists = users.some(user => user.email === email || user.username === username);
     if (userExists) {
-        alert('Cet email est déjà utilisé.');
+        alert('Cet email ou nom d\'utilisateur est déjà utilisé.');
         return;
     }
 
-    const newUser = { firstName, lastName, email, password };
+    const newUser = { username, email, password };
     users.push(newUser);
     localStorage.setItem('users', JSON.stringify(users));
-    localStorage.setItem('userFirstName', firstName);
-
+    localStorage.setItem('userFirstName', username);
     alert('Inscription réussie ! Vous êtes maintenant connecté.');
     updateHeader();
-
     setTimeout(function() {
         window.location.href = 'accueil.html';
     }, 10);
@@ -77,26 +68,23 @@ function handleInscriptionSubmit(event) {
 
 function handleConnexionSubmit(event) {
     event.preventDefault();
-    
     resetErrorStyles();
-
-    const email = document.getElementById('emailConnexion').value;
+    const username = document.getElementById('usernameConnexion').value;
     const password = document.getElementById('passwordConnexion').value;
 
     const users = JSON.parse(localStorage.getItem('users')) || [];
-    const user = users.find(user => user.email === email);
+    const user = users.find(user => user.username === username);
 
     if (!user) {
-        showError('emailConnexion', 'Email invalide.');
+        showError('usernameConnexion', 'Nom d\'utilisateur invalide.');
         return;
     } else if (user.password !== password) {
         showError('passwordConnexion', 'Mot de passe incorrect.');
         return;
     } else {
-        localStorage.setItem('userFirstName', user.firstName);
+        localStorage.setItem('userFirstName', user.username);
         alert('Connexion réussie !');
         updateHeader();
-        
         setTimeout(function() {
             window.location.href = 'accueil.html';
         }, 10);
@@ -106,20 +94,41 @@ function handleConnexionSubmit(event) {
 function showError(fieldId, message) {
     const field = document.getElementById(fieldId);
     const errorField = document.createElement('div');
-    
     errorField.className = 'error-message';
     errorField.innerText = message;
-
-    field.classList.add('error'); 
-    field.parentNode.appendChild(errorField); 
+    field.classList.add('error');
+    field.parentNode.appendChild(errorField);
 }
 
 function resetErrorStyles() {
     document.querySelectorAll('.error-message').forEach(function(message) {
         message.remove();
     });
-
     document.querySelectorAll('.error').forEach(function(field) {
         field.classList.remove('error');
     });
+}
+
+function togglePassword() {
+    togglePasswordVisibility('passwordConnexion', 'toggleIconConnexion');
+}
+
+function togglePasswordInscription() {
+    togglePasswordVisibility('passwordInscription', 'toggleIconInscription');
+}
+
+function toggleConfirmPasswordInscription() {
+    togglePasswordVisibility('confirmPasswordInscription', 'toggleConfirmIconInscription');
+}
+
+function togglePasswordVisibility(passwordFieldId, toggleIconId) {
+    const passwordField = document.getElementById(passwordFieldId);
+    const toggleIcon = document.getElementById(toggleIconId);
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        toggleIcon.setAttribute('name', 'show');
+    } else {
+        passwordField.type = "password";
+        toggleIcon.setAttribute('name', 'hide');
+    }
 }
